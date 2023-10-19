61bda1d119d10e9a055f7457f861ec42d3b210be
trahongquan <lovenevertolate97@gmail.com>
2023-10-19 20:36:45 +0700

    First commit

---
+++ x/QLTTBCNTTDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace QLTTBCNTT_WinForm
+{
+
+
+    partial class QLTTBCNTTDataSet
+    {
+    }
+}
+
+namespace QLTTBCNTT_WinForm.QLTTBCNTTDataSetTableAdapters {
+    
+    
+    public partial class DM_ThietBiTableAdapter {
+    }
+}
